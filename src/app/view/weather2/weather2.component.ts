34adbf200de9f76cb46fa5777473a161fbc8fad4
BDenisss <bucspun.d@gmail.com>
2023-12-20 11:25:42 +0100

    :sparkles: convertEpochToDay's function

--- src/app/view/weather2/weather2.component.ts
+++ src/app/view/weather2/weather2.component.ts
@@ -239,6 +239,30 @@ export class Weather2Component implements OnInit {
     return date.getHours();
   }
 
+  convertEpochToDay(epoch: number) {
+    const date = new Date(epoch * 1000);
+    const day = date.getDay();
+
+    switch (day) {
+      case 0:
+        return 'Dimanche';
+      case 1:
+        return 'Lundi';
+      case 2:
+        return 'Mardi';
+      case 3:
+        return 'Mercredi';
+      case 4:
+        return 'Jeudi';
+      case 5:
+        return 'Vendredi';
+      case 6:
+        return 'Samedi';
+      default:
+        return 'Jour inconnu';
+    }
+  }
+
   getCurrentTime() {
     const date = new Date();
     console.log("l'heure actuelle : " + date.getHours() + "h");
